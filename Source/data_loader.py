45b39d6436060e2bfb765729345854f9066d8847
bunyod123 <bunyodbek9690@gmail.com>
2025-10-12 19:40:31 +0900

    s

---
+++ Source/data_loader.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+class data_load:
+
+    def __init__(self, path : str):
+        self.path = path
+
+    def load(self):
+        df = pd.read_csv(self.path)
+        return df
+
+#------------------------------------------------------
+# import pandas as pd
+
+# class DataLoader:
+#     def __init__(self, path: str):
+#         self.path = path
+
+#     def load_data(self) -> pd.DataFrame:
+#         """CSV faylni yuklash"""
+#         try:
+#             df = pd.read_csv(self.path)
+#             print(f"[INFO] Dataset muvaffaqiyatli yuklandi: {self.path}")
+#             return df
+#         except Exception as e:
+#             print(f"[ERROR] Fayl yuklanmadi: {e}")
+#             return None

c1e6f76de6918955b9e4e20ffc966f4ee3f45914
bunyod123 <bunyodbek9690@gmail.com>
2025-10-13 23:46:15 +0900

    s

---
+++ Source/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+def get_logger(name: str, log_file: str = "project.log"):
+    # log fayllar uchun papka
+    os.makedirs("logs", exist_ok=True)
+    log_path = os.path.join("logs", log_file)
+
+    # logging konfiguratsiyasi — faqat bir marta yaratiladi
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_path, encoding='utf-8'),
+            logging.StreamHandler()
+        ]
+    )
+
+    # har bir modul uchun o‘z nomli logger
+    logger = logging.getLogger(name)
+    return logger

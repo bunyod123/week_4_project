284d927ff48e40431dc5b9a03a9e8e118ec42448
bunyod123 <bunyodbek9690@gmail.com>
2025-10-16 13:02:38 +0900

    added

--- Scripts/data_load.py
+++ Scripts/data_load.py
@@ -4,10 +4,14 @@ sys.path.append(r"C:\Users\bunyo\OneDrive\Desktop\git_project\4_week_project")
 from Source.data_loader import data_load
 from Source.logger import get_logger
 
-logger = get_logger("data_loading", 'data_loading.log')
 
+
+#----------------------------------------------------------------------------------------------
+logger = get_logger("data_loading", 'data_loading.log')
 path = r"C:\Users\bunyo\OneDrive\Desktop\git_project\4_week_project\Data\raw_data\cwur_2025_rankings.csv"
 
+
+#---------------------------------------------------------------------------------------------------
 try:
     df = data_load(path)
     logger.info('Data set script file orqali yuklandi')
--- Scripts/data_preprocessing.py
+++ Scripts/data_preprocessing.py
@@ -1,18 +1,19 @@
-import sys
-
-sys.path.append(r"C:\Users\bunyo\OneDrive\Desktop\git_project\4_week_project")
-
-
+import sys,os
 import pandas as pd
 from Source.preprocessing import Preprocessing
 from Source.logger import get_logger
+#----------------------------------------------------------------------------------------------
 
+sys.path.append(r"C:\Users\bunyo\OneDrive\Desktop\git_project\4_week_project")
 logger = get_logger('use_preprocessing', 'preprocessing.log')
 
+#------------------------------------------------------------------------------------------------
 # Dataset yuklash
 df = pd.read_csv('data/dataset.csv')
 logger.info(f"Dataset yuklandi: {df.shape}")
 
+
+#--------------------------------------------------------------------------------------------------
 # Preprocessing obyekt yaratish
 pre = Preprocessing(df)
 
@@ -24,3 +25,19 @@ df_clean = (
        .logTransformation()
        .getDataset()
 )
+
+
+logger.info(f"Preprocessing tugadi. Shape: {df_clean.shape}")
+
+#--------------------------------------------------------------------------------------------------
+# faylni saqlaymiz
+path = r"C:\Users\bunyo\OneDrive\Desktop\git_project\4_week_project\Data\preprocessed_data"
+output_dir = os.path.join(path, "preprocessed")
+os.makedirs(output_dir, exist_ok=True)
+
+output_path = os.path.join(output_dir, "cleaned_dataset.csv")
+df_clean.to_csv(output_path, index=False)
+
+logger.info(f"Tozalangan dataset saqlandi: {output_path}")
+print(f"Fayl saqlandi: {output_path}")
+#----------------------------------------------------------------------------------------------------
